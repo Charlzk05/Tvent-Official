c7bf33cf8b8baf5ad308527cb8999e9f85579a5d
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-09-14 06:40:09 +0000

    Updated server.js and main.ejs

--- server.js
+++ server.js
@@ -1,40 +1,42 @@
 const { TwitterApi } = require("twitter-api-v2");
 const express = require('express');
-const app = express();
 require("dotenv").config();
 
+const app = express();
 const PORT = 8000
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
+
 app.use(express.static(__dirname + '/views/resources'));
 
 app.get("/", function(req, res) {
-    res.render("main");
+  res.render("main");
 });
 
-app.get('/vent', function (req, res) {
-    const client = new TwitterApi({
-        appKey: process.env.API_KEY,
-        appSecret: process.env.API_KEY_SECRET,
-        accessToken: process.env.ACCESS_TOKEN,
-        accessSecret: process.env.ACCESS_TOKEN_SECRET
-    });
-    const readWriteClient = client.readWrite;
-    const content = `${req.query.content}\n\n- Anonymous`
+app.get('/vent', function(req, res) {
+  const client = new TwitterApi({
+    appKey: process.env.API_KEY,
+    appSecret: process.env.API_KEY_SECRET,
+    accessToken: process.env.ACCESS_TOKEN,
+    accessSecret: process.env.ACCESS_TOKEN_SECRET,
+  });
 
-    if (req.query.content == "") {
+  const readWriteClient = client.readWrite;
+  const content = `${req.query.content}\n\n- Anonymous`
 
-    }
+  if (req.query.content == "") {
 
+  } else {
     if (res.statusCode == 200) {
-        res.render("submitted", { content: req.query.content, serverLog: "Status Code: 200 - The tweet was successful :D" });
-        readWriteClient.v2.tweet(content);
+      res.render("submitted", { content: req.query.content, serverLog: "Status Code: 200 - The tweet was successful :D" });
+      readWriteClient.v2.tweet(content);
     } else {
-        res.render("submitted", { content: req.query.content, serverLog: `Status Code: ${res.statusCode} - The tweet was not successful, Something went wrong ...` });
+      res.render("submitted", { content: req.query.content, serverLog: `Status Code: ${res.statusCode} - The tweet was not successful, Something went wrong ...` });
     }
+  }
 });
 
 app.listen(PORT, function() {
-    console.log("Express server is listening to " + PORT);
+  console.log("Express server is listening to " + PORT);
 });
